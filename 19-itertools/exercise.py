fbf51e0b01ee5c98748b426f26321d4a646bf06f
jean.dossantos <jeandsantos88@gmail.com>
2023-10-24 19:41:11 +0200

    added itertools exercise

---
+++ 19-itertools/exercise.py
@@ -0,0 +1,50 @@
+import itertools
+import operator
+import random
+from collections import Counter
+from dataclasses import dataclass
+
+from faker import Faker
+
+
+@dataclass
+class Person:
+    name: str
+    age: int
+    city: str
+    country: str
+
+
+# Instantiate the Faker module
+fake = Faker()
+
+# List of possible countries
+countries = [
+    "UK",
+    "USA",
+    "Japan",
+    "Australia",
+    "France",
+    "Germany",
+    "Italy",
+    "Spain",
+    "Canada",
+    "Mexico",
+]
+
+# Generate 1000 random Person instances
+PERSON_DATA: list[Person] = [
+    Person(fake.name(), random.randint(18, 70), fake.city(), random.choice(countries)) for _ in range(1000)
+]
+
+
+def main() -> None:
+    filtered_data = list(itertools.filterfalse(lambda user: user.age < 21, PERSON_DATA))
+
+    summary = Counter([person.country for person in filtered_data])
+
+    print(summary)
+
+
+if __name__ == "__main__":
+    main()
